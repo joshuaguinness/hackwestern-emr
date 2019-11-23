b96dd49c5bd8ba0fa44de6d396fa714b14b4e37e
Joshua Guinness <joshua.guinness@gmail.com>
2019-11-23 15:01:08 -0500

    Reorganized files and added files for implementing stich

---
+++ emr-app/components/patients.js
@@ -0,0 +1,7 @@
+// import { patients } from "../stich/mongodb";
+
+        
+// export function patients(userId) {
+//   const [state, dispatch] = React.useReducer(todoReducer, { todos: [] });
+//   const viewPatient = async 
+// }
---
+++ emr-app/stich/app.js
@@ -0,0 +1,11 @@
+// import { Stitch } from "mongodb-stitch-browser-sdk";
+
+// // TODO: Add your Stitch app's App ID
+// const APP_ID = "hackwester-emr-iaxfo";
+
+// // TODO: Initialize the app client
+// const app =titch.hasAppClient(APP_ID)
+// ? Stitch.getAppClient(APP_ID)
+// : Stitch.initializeAppClient(APP_ID);
+
+// export { app };
